ba2dca714a17645510a16d3c4fc270966342915b
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2020-11-12 09:29:23 -0700

    Update ExampleMain.java

--- src/com/lingoport/demo/java/ExampleMain.java
+++ src/com/lingoport/demo/java/ExampleMain.java
@@ -52,7 +52,7 @@ public class ExampleMain {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ExamplePanel panel = new ExamplePanel();
         frame.getContentPane().add(panel);
-	changeFont(frame, new Font("Courier", Font.BOLD,28));
+	changeFont(frame, new Font("Courier", Font.BOLD,28)); // $NON-NLS-L$
         frame.pack();
         frame.setVisible(true);
         String strTitle = "Example Address Book -- " + locale;

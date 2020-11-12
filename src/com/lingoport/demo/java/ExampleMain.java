2a4d5c55064f7852459dc6c1bbe7b22bd2321d16
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2020-11-12 09:51:28 -0700

    Update ExampleMain.java

--- src/com/lingoport/demo/java/ExampleMain.java
+++ src/com/lingoport/demo/java/ExampleMain.java
@@ -53,7 +53,7 @@ public class ExampleMain {
 	changeFont(frame, new Font("Courier", Font.BOLD,28));
         frame.pack();
         frame.setVisible(true);
-        String strTitle = "Address Book -- " + locale;
+        String strTitle = "Addresses -- " + locale;
         frame.setTitle(strTitle);
     }
 	
